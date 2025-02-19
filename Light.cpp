7a3f480a7ab3031f3ea66d3126fd77fe3175e79f
sakuma-shin <k023g0084@g.neec.ac.jp>
2025-02-19 16:50:24 +0900

    ワールドトランスフォームにアフィン変換を追加

---
+++ Light.cpp
@@ -0,0 +1,54 @@
+#include "Light.h"
+#include <cassert>
+
+using namespace KamataEngine;
+
+void Light::Initialize(Sprite* sprite, uint32_t textureHandle) {
+	// NULLポインタチェック
+	assert(sprite);
+	sprite_ = sprite;
+	textureHandle_ = textureHandle;
+	worldTransform_.Initialize();
+}
+
+void Light::Update() {
+	Grow();
+
+	worldTransform_.
+}
+
+void Light::Draw() {}
+
+void Light::Grow() {
+	float kSpeed = 0.1f;
+	switch (growtype) {
+	case Up:
+		
+		velocity_ = {0.0f, -kSpeed, 0.0f};
+
+		break;
+
+	case Down:
+		velocity_ = {0.0f, kSpeed, 0.0f};
+
+		break;
+
+	case Left:
+		velocity_ = {-kSpeed, 0.0f, 0.0f};
+
+		break;
+
+	case Right:
+		velocity_ = {kSpeed, 0.0f, 0.0f};
+
+		break;
+
+	case NO:
+		velocity_ = {0.0f, 0.0f, 0.0f};
+
+		break;
+	}
+
+	width_ += velocity_.x;
+	height_ += velocity_.y;
+}
