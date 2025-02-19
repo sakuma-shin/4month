e0a563b48f0326b54bca6c85d984324fb10ca9b0
sakuma-shin <k023g0084@g.neec.ac.jp>
2025-02-19 15:40:52 +0900

    ゲームシーンを追加

---
+++ GameScene.h
@@ -0,0 +1,33 @@
+#pragma once
+#include "KamataEngine.h"
+
+using namespace KamataEngine;
+
+class GameScene {
+public:
+	// コンストラクタ
+	GameScene();
+
+	// デストラクタ
+	~GameScene();
+
+	void Initialize();
+
+	void Update();
+
+	void Draw();
+
+private:
+	DirectXCommon* dxCommon_ = nullptr;
+	Input* input_ = nullptr;
+	Audio* audio_ = nullptr;
+
+	// テクスチャハンドル
+	uint32_t playerTextureHandle_ = 0;
+
+	// モデル
+	Model* playerModel_ = nullptr;
+
+	// カメラ
+	Camera camera_;
+};
