de24c0fcd87a2727bf59e41caa0a2daf0e3f5f7e
onknjpeel <k023g0103@g.neec.ac.jp>
2025-03-17 10:11:10 +0900

    プリズム破壊処理・続き

--- Prism.cpp
+++ Prism.cpp
@@ -1,8 +1,15 @@
 #include "Prism.h"
 
-void Prism::Initialize(int key, WorldTransform* worldTransform) {
+void Prism::Initialize(int key, WorldTransform* worldTransform, int x, int z, Map* map) {
 	key_ = key;
 	worldTransform_ = worldTransform;
+
+	pos_[0] = x;
+	pos_[1] = z;
+
+	map_ = map;
+
+	input_ = Input::GetInstance();
 
 	// keyに応じて対応したモデルを読み、自身の向きを決定する
 	if (key_ == 91) {
@@ -23,9 +30,26 @@ void Prism::Initialize(int key, WorldTransform* worldTransform) {
 	}
 }
 
-void Prism::Update() {
-	ImGui::Begin("prism");
+void Prism::Update(Player* player) {
+	player_ = player;
+
+	if (map_->CheckCollision(Vector3(worldTransform_->translation_.x, worldTransform_->translation_.y, worldTransform_->translation_.z + 2.0f)) == 0 ||
+	    map_->CheckCollision(Vector3(worldTransform_->translation_.x - 2.0f, worldTransform_->translation_.y, worldTransform_->translation_.z)) == 0 ||
+	    map_->CheckCollision(Vector3(worldTransform_->translation_.x, worldTransform_->translation_.y, worldTransform_->translation_.z - 2.0f)) == 0 ||
+	    map_->CheckCollision(Vector3(worldTransform_->translation_.x + 2.0f, worldTransform_->translation_.y, worldTransform_->translation_.z)) == 0) {
+		if (input_->TriggerKey(DIK_K)) {
+			if (isSet_) {
+				Broken();
+			}
+		}
+	}
+
+	int count = player_->GetCount();
+
+	std::string windowName = "Prism_" + std::to_string(reinterpret_cast<uintptr_t>(this));
+	ImGui::Begin(windowName.c_str());
 	ImGui::InputInt("key", &key_);
+	ImGui::InputInt("count", &count);
 	if (ImGui::Button("broken", {50, 50})) {
 		Broken();
 	}
@@ -35,18 +59,20 @@ void Prism::Update() {
 	ImGui::End();
 }
 
-void Prism::Draw( Camera* camera) {
+void Prism::Draw(Camera* camera) {
 	if (isSet_) {
 		model_->Draw(*worldTransform_, *camera);
 	}
 }
 
-int Prism::CheckLight2Direction() { return 1; }
-
-void Prism::ReflectLight() {}
-
 void Prism::DecompositionLight() {}
 
-void Prism::Broken() { isSet_ = false; }
+void Prism::Broken() {
+	isSet_ = false;
+	player_->SetCount(1);
+}
 
-void Prism::Set() { isSet_ = true; }
+void Prism::Set() {
+	isSet_ = true;
+	player_->SetCount(0);
+}
