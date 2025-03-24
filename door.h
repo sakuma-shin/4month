bb2ac3c69f8127b9b036d9477b12f0818d4eb74f
Noir-0409 <146914778+Noir-0409@users.noreply.github.com>
2025-03-24 16:42:47 +0900

    修正

--- door.h
+++ door.h
@@ -1,31 +1,3 @@
-//#pragma once
-//#include <KamataEngine.h>
-//#include "3D/Model.h"
-//#include "3D/WorldTransform.h"
-//#include "3D/camera.h"
-//#include"math/Vector2.h"
-//#include "Target.h"
-//
-//class door {
-//public:
-//	void Initialize(int key, std::vector<Target*> target);
-//	void Update(std::vector<Target*> target);
-//	void Draw(KamataEngine::WorldTransform* worldtransform, KamataEngine::Camera* camera);
-//	bool keyopen();
-//	int Digit(int number);
-//
-//	int Digitnamber(int number);
-//
-//	int UnFirstnumber(int number);
-//
-//private:
-//	KamataEngine::Model* doormodel_;
-//	bool openflag = false;
-//	int key_;
-//	std::vector<Target*> target_;
-//};
-
-
 #pragma once
 #include <KamataEngine.h>
 #include "3D/Model.h"
