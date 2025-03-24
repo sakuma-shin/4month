f0469bb2aa4ec49ca942045ea5346734ed18d8a8
Noir-0409 <146914778+Noir-0409@users.noreply.github.com>
2025-03-24 10:22:06 +0900

    ドアの開閉処理

--- Map.cpp
+++ Map.cpp
@@ -66,7 +66,7 @@ void Map::Initialize(KamataEngine::Model* model, uint32_t textureHandle, KamataE
 	for (uint32_t i = 0; i < MaxX * MaxY; ++i) {
 		if (Digit(map[i % MaxX][i / MaxX]) == 7) {
 			door* newdoor = new door;
-			newdoor->Initialize(UnFirstnumber(map[i % MaxX][i / MaxX]), target_);
+			newdoor->Initialize(UnFirstnumber(map[i % MaxX][i / MaxX]), target_, i % MaxX, i / MaxX);
 			door_.push_back(newdoor);
 		}
 		if (map[i % MaxX][i / MaxX] >= 30 && map[i % MaxX][i / MaxX] <= 34) {
@@ -103,9 +103,35 @@ void Map::Update(Player* player) {
 
 	colorGlassCount = 0;
 
+	/*for (door* door : door_) {
+		door->Update(target_);
+	}*/
+
 	for (door* door : door_) {
 		door->Update(target_);
+
+		if (door->IsOpen()) {
+
+			int doorX = door->getpos(0);
+			int doorZ = door->getpos(1);
+
+			map[doorX][doorZ] = 0;
+
+		} else {
+
+			int doorX = door->getpos(0);
+			int doorZ = door->getpos(1);
+
+			if (doorX == 4 && doorZ == 4) {
+				map[doorX][doorZ] = 71;  // ドア1を元に戻す
+			} else if (doorX == 4 && doorZ == 11) {
+				map[doorX][doorZ] = 723; // ドア2を元に戻す
+			}
+
+		}
+
 	}
+
 	for (Target* target : target_) { //
 		target->Update();
 	}
@@ -306,6 +332,66 @@ int Map::CheckCollision(KamataEngine::Vector3 pos) { // マップのX,Z座標を
 		switch (map[mapX][mapZ]) {
 
 			// マップ番号と同じ数字を返す
+		//case 8:
+		//	// 壁
+		//	return 8;
+		//	break;
+
+		//case 31:
+		//	// 右下鏡
+		//	return 31;
+		//	break;
+
+		//case 32:
+		//	// 右上鏡
+		//	return 32;
+		//	break;
+
+		//case 33:
+		//	// 水平鏡
+		//	return 33;
+		//	break;
+
+		//case 34:
+		//	// 垂直鏡
+		//	return 34;
+		//	break;
+
+		//case 52:
+		//	//紫ガラス
+		//	return 52;
+		//	break;
+
+		//case 91:
+		//	// プリズム上
+		//	return 91;
+		//	break;
+
+		//case 92:
+		//	// プリズム下
+		//	return 92;
+		//	break;
+
+		//case 93:
+		//	// プリズム左
+		//	return 93;
+		//	break;
+
+		//case 94:
+		//	// プリズム右
+		//	return 94;
+		//	break;
+
+		//case 42:
+		//	// 垂直鏡
+		//	return 42;
+		//	break;
+		
+		case 2:
+			//ゴール
+			return 2;
+			break;
+
 		case 8:
 			// 壁
 			return 8;
@@ -331,9 +417,84 @@ int Map::CheckCollision(KamataEngine::Vector3 pos) { // マップのX,Z座標を
 			return 34;
 			break;
 
+		case 41:
+			//上向きライト
+			return 41;
+			break;
+
+		case 42:
+			//下向きライト
+			return 42;
+			break;
+
+		case 43:
+			//左向きライト
+			return 43;
+			break;
+
+		case 44:
+			//右向きライト
+			return 44;
+			break;
+
+		case 51:
+			//色なしガラス
+			return 51;
+			break;
+
 		case 52:
 			//紫ガラス
 			return 52;
+			break;
+
+		case 53:
+			//緑ガラス
+			return 53;
+			break;
+
+		case 54:
+			//橙ガラス
+			return 54;
+			break;
+
+		case 61:
+			//色無しセンサー
+			return 61;
+			break;
+
+		case 62:
+			//紫センサー
+			return 62;
+			break;
+
+		case 63:
+			//緑センサー
+			return 63;
+			break;
+
+		case 64:
+			//橙センサー
+			return 64;
+			break;
+
+		case 65:
+			//赤センサー
+			return 65;
+			break;
+
+		case 66:
+			//青センサー
+			return 66;
+			break;
+
+		case 67:
+			//黄センサー
+			return 67;
+			break;
+
+		case 71:
+			//ドア
+			return 71;
 			break;
 
 		case 91:
@@ -356,10 +517,22 @@ int Map::CheckCollision(KamataEngine::Vector3 pos) { // マップのX,Z座標を
 			return 94;
 			break;
 
-		case 42:
-			// 垂直鏡
-			return 42;
+		case 621:
+			return 621;
 			break;
+
+		case 653:
+			return 653;
+			break;
+
+		case 662:
+			return 662;
+			break;
+
+		case 723:
+			return 723;
+			break;
+		
 		}
 	}
 
@@ -392,7 +565,7 @@ void Map::Reorldtransform() {
 
 		if (Digit(map[i % MaxX][i / MaxX]) == 7) {
 			door* newdoor = new door;
-			newdoor->Initialize(UnFirstnumber(map[i % MaxX][i / MaxX]), target_);
+			newdoor->Initialize(UnFirstnumber(map[i % MaxX][i / MaxX]), target_, i % MaxX, i / MaxX);
 			door_.push_back(newdoor);
 		}
 		if (map[i % MaxX][i / MaxX] >= 30 && map[i % MaxX][i / MaxX] <= 34) {
