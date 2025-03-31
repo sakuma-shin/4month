27fd6905e3ceea41895de5b8258132fc3b76d40a
onozawakazutoshi <146906422+onozawakazutoshi@users.noreply.github.com>
2025-03-31 15:45:46 +0900

    //

--- Map.cpp
+++ Map.cpp
@@ -102,9 +102,9 @@ void Map::Update(Player* player) {
 	prismcount = 0;
 
 	colorGlassCount = 0;
-	//int doorC = 0;
+	// int doorC = 0;
 	for (door* door : door_) {
-		
+
 		door->Update(target_);
 
 		if (door->IsOpen()) {
@@ -120,17 +120,17 @@ void Map::Update(Player* player) {
 			int doorZ = door->getpos(1);
 
 			if (doorX == 4 && doorZ == 4) {
-				map[doorX][doorZ] = 71;  // ドア1を元に戻す
+				map[doorX][doorZ] = 71; // ドア1を元に戻す
 			} else if (doorX == 4 && doorZ == 11) {
 				map[doorX][doorZ] = 723; // ドア2を元に戻す
 			}
 
-		if (door->Getopenflag() && map[door->Getpos(0)][door->Getpos(1)]!=0) {
-			map[door->Getpos(0)][door->Getpos(1)] = 0;
-		} else if (!door->Getopenflag()) {
-			map[door->Getpos(0)][door->Getpos(1)] = door->Getnumber();
+			if (door->Getopenflag() && map[door->Getpos(0)][door->Getpos(1)] != 0) {
+				map[door->Getpos(0)][door->Getpos(1)] = 0;
+			} else if (!door->Getopenflag()) {
+				map[door->Getpos(0)][door->Getpos(1)] = door->Getnumber();
+			}
 		}
-
 	}
 
 	for (Target* target : target_) { //
@@ -336,160 +336,162 @@ int Map::CheckCollision(KamataEngine::Vector3 pos) { // マップのX,Z座標を
 
 		// その位置のマップ値が 8 なら壁
 		return map[mapX][mapZ];
-		//switch (map[mapX][mapZ]) {
+		// switch (map[mapX][mapZ]) {
 
-			// マップ番号と同じ数字を返す
+		// マップ番号と同じ数字を返す
 
-		//case 2:
+		// case 2:
 		//	//ゴール
 		//	return 2;
 		//	break;
 
-		case 8:
-			// 壁
-			return 8;
-			break;
+		//	case 8:
+		//		// 壁
+		//		return 8;
+		//		break;
 
-		//case 31:
-		//	// 右下鏡
-		//	return 31;
-		//	break;
+		//	//case 31:
+		//	//	// 右下鏡
+		//	//	return 31;
+		//	//	break;
 
-		//case 32:
-		//	// 右上鏡
-		//	return 32;
-		//	break;
+		//	//case 32:
+		//	//	// 右上鏡
+		//	//	return 32;
+		//	//	break;
 
-		//case 33:
-		//	// 水平鏡
-		//	return 33;
-		//	break;
+		//	//case 33:
+		//	//	// 水平鏡
+		//	//	return 33;
+		//	//	break;
 
-		//case 34:
-		//	// 垂直鏡
-		//	return 34;
-		//	break;
+		//	//case 34:
+		//	//	// 垂直鏡
+		//	//	return 34;
+		//	//	break;
 
-		case 41:
-			//上向きライト
-			return 41;
-			break;
+		//	case 41:
+		//		//上向きライト
+		//		return 41;
+		//		break;
 
-		case 42:
-			//下向きライト
-			return 42;
-			break;
+		//	case 42:
+		//		//下向きライト
+		//		return 42;
+		//		break;
 
-		case 43:
-			//左向きライト
-			return 43;
-			break;
+		//	case 43:
+		//		//左向きライト
+		//		return 43;
+		//		break;
 
-		case 44:
-			//右向きライト
-			return 44;
-			break;
+		//	case 44:
+		//		//右向きライト
+		//		return 44;
+		//		break;
 
-		case 51:
-			//色なしガラス
-			return 51;
-			break;
+		//	case 51:
+		//		//色なしガラス
+		//		return 51;
+		//		break;
 
-		case 52:
-			// 紫ガラス
-			return 52;
-			break;
+		//	case 52:
+		//		// 紫ガラス
+		//		return 52;
+		//		break;
 
-		case 53:
-			//緑ガラス
-			return 53;
-			break;
+		//	case 53:
+		//		//緑ガラス
+		//		return 53;
+		//		break;
 
-		case 54:
-			//橙ガラス
-			return 54;
-			break;
+		//	case 54:
+		//		//橙ガラス
+		//		return 54;
+		//		break;
 
-		case 61:
-			//色無しセンサー
-			return 61;
-			break;
+		//	case 61:
+		//		//色無しセンサー
+		//		return 61;
+		//		break;
 
-		case 62:
-			//紫センサー
-			return 62;
-			break;
+		//	case 62:
+		//		//紫センサー
+		//		return 62;
+		//		break;
 
-		case 63:
-			//緑センサー
-			return 63;
-			break;
+		//	case 63:
+		//		//緑センサー
+		//		return 63;
+		//		break;
 
-		case 64:
-			//橙センサー
-			return 64;
-			break;
+		//	case 64:
+		//		//橙センサー
+		//		return 64;
+		//		break;
 
-		case 65:
-			//赤センサー
-			return 65;
-			break;
+		//	case 65:
+		//		//赤センサー
+		//		return 65;
+		//		break;
 
-		case 66:
-			//青センサー
-			return 66;
-			break;
+		//	case 66:
+		//		//青センサー
+		//		return 66;
+		//		break;
 
-		case 67:
-			//黄センサー
-			return 67;
-			break;
+		//	case 67:
+		//		//黄センサー
+		//		return 67;
+		//		break;
 
-		case 71:
-			//ドア
-			return 71;
-			break;
+		//	case 71:
+		//		//ドア
+		//		return 71;
+		//		break;
 
-		case 91:
-			// プリズム上
-			return 91;
-			break;
+		//	case 91:
+		//		// プリズム上
+		//		return 91;
+		//		break;
 
-		//case 93:
-		//	// プリズム左
-		//	return 93;
-		//	break;
+		//	//case 93:
+		//	//	// プリズム左
+		//	//	return 93;
+		//	//	break;
 
-		//case 94:
-		//	// プリズム右
-		//	return 94;
-		//	break;
+		//	//case 94:
+		//	//	// プリズム右
+		//	//	return 94;
+		//	//	break;
 
-		//case 42:
-		//	// 垂直鏡
-		//	return 42;
-		//	break;
+		//	//case 42:
+		//	//	// 垂直鏡
+		//	//	return 42;
+		//	//	break;
 
-		case 621:
-			return 621;
-			break;
+		//	case 621:
+		//		return 621;
+		//		break;
 
-		case 653:
-			return 653;
-			break;
+		//	case 653:
+		//		return 653;
+		//		break;
 
-		case 662:
-			return 662;
-			break;
+		//	case 662:
+		//		return 662;
+		//		break;
 
-		case 723:
-			return 723;
-			break;
+		//	case 723:
+		//		return 723;
+		//		break;
+		//
+		//	}
+		//}
+
+		// 範囲内かつ衝突しない場合は「衝突なし」
 		
-		}
 	}
-
-	// 範囲内かつ衝突しない場合は「衝突なし」
 	return 0;
 }
 
