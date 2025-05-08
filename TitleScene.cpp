d4403e38b5c85ce4e57c7ee865ac53d8ce57b544
a <hogehoge@example.com>
2025-05-08 15:30:47 +0900

    なんかできた

--- TitleScene.cpp
+++ TitleScene.cpp
@@ -12,23 +12,55 @@ void TitleScene::Initialize() {
 
 	sceneChangeSoundHandle_ = audio_->LoadWave("sounds/firstMusic.mp3");
 
+	
+	fade_ = new Fade();
+	fade_->Initialize();
+	fade_->Start(Fade::Status::FadeIn, 1.0f);
+
+
 }
 
 void TitleScene::Update() {
 
 	if (input_->TriggerKey(DIK_SPACE)) {
 
-		isFinished_ = true;
+	/*	isFinished_ = true;*/
 		if (audio_->IsPlaying(sceneChangeSoundHandle_)) {
 			audio_->PlayWave(sceneChangeSoundHandle_,false,0.5f);
 		}
 	}
+
+	switch (phase_) {
+
+	case FadePhase::kFadeIn:
+		if (fade_->isFinished()) {
+			phase_ = FadePhase::kMain;
+		}
+		break;
+
+	case FadePhase::kMain:
+		if (input_->TriggerKey(DIK_SPACE)) {
+			phase_ = FadePhase::kfadeOut;
+			fade_->Start(Fade::Status::FadeOut, 1.0f);
+		}
+		break;
+
+	case FadePhase::kfadeOut:
+		if (fade_->isFinished()) {
+			isFinished_ = true;
+		}
+		break;
+	}
+
+	fade_->Update();
 }
+
 
 void TitleScene::Draw() { 
 	if (this != nullptr) {
 		Sprite::PreDraw(dxCommon_->GetCommandList());
 		sprite_->Draw();
+		fade_->Draw(dxCommon_->GetCommandList());
 		Sprite::PostDraw();
 	}
 }
