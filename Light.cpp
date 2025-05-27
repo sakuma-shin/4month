3825ccc31ae42e47a8933505725ee65b0cbd12cd
onozawakazutoshi <146906422+onozawakazutoshi@users.noreply.github.com>
2025-05-27 14:00:43 +0900

    //

--- Light.cpp
+++ Light.cpp
@@ -330,7 +330,7 @@ Vector3 Light::GetEndPosition() {
 			return {worldTransform_.translation_.x + worldTransform_.scale_.x + 0.5f, 0.0f, worldTransform_.translation_.z + worldTransform_.scale_.z};
 		}
 		if (newType_ == Right) {
-			return {worldTransform_.translation_.x +worldTransform_.scale_.x-1.7f, 0.0f, worldTransform_.translation_.z + worldTransform_.scale_.z + 0.5f};
+			return {worldTransform_.translation_.x +worldTransform_.scale_.x-1.0f, 0.0f, worldTransform_.translation_.z + worldTransform_.scale_.z + 0.5f};
 		}
 		if (newType_ == Down) {
 			return {worldTransform_.translation_.x + worldTransform_.scale_.x + 0.5f, 0.0f, worldTransform_.translation_.z + worldTransform_.scale_.z + 0.5f};
